f80cf425baab0a1622cd6afc29789fbf15eb239e
Aniket-codes17 <your-email@example.com>
2026-07-31 21:42:03 +0530

    add venv, requirements.txt and .gitignore

---
+++ learn.py
@@ -0,0 +1,20 @@
+print("hello lets xyyxyxv")
+name='john'
+age = 45
+temp=101
+print(name, 'is', age , 'years old his temp is', temp )
+symptoms=['fever', 'cough']
+for s in symptoms:
+    print("symptoms:",s)
+
+    patient = {"age": 45, "gender": "M"}
+print(patient["age"])
+if temp > 100:
+    print("high fever")
+else:
+    print("normal")
+
+def greet(name):
+    return "Hello " + name
+print(greet("Doctor"))
+print(greet("Nurse"))
